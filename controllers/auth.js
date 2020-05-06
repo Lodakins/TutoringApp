5d82d651805deb44384b815795e5d431a7c8f1e3
Akinrolabu Lolade <l.akinrolabu@csdcconsulting.com>
2020-05-07 00:47:04 +0100

    modified

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,16 @@ const jwt = require("jsonwebtoken");
 
 
 exports.register=(req,res,next)=>{
-    
+   const email = req.body.email;
+   const firstName = req.body.firstName;
+   const lastName = req.body.lastName;
+   const password = req.body.password;
+   const gender = req.body.gender;
+   const phoneNumber = req.body.phoneNumber;
+   const role = req.body.role;
+   const category = req.body.category;
+
+   
 
 
 
--- models/user.js
+++ models/user.js
@@ -2,21 +2,37 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const userSchema = new schema({
-    firstname:{
-
+    firstName:{
+        type:String,
+        required:true
     },
-    lastname:{
-
-    },
-    category:{
-        
-    }
-    email:{
+    lastName:{
         type:String,
         required:true
     },
     password:{
         type:String,
         required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:String
+    },
+    role:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
     }
 }, {timestamps:true});
+
+module.exports = mongoose.model("User",userSchema)
