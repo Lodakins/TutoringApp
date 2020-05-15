a63d95e16468b0b59232dd4eaaf4de2a54431a38
Akinrolabu Lolade <l.akinrolabu@csdcconsulting.com>
2020-05-15 22:05:11 +0100

    modified

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ const app = express();
 
 const apiKey = process.env.API_KEY;
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use("/v1",authRoutes);
 
 
-mongoose.connect(`mongodb+srv://lodakins99:${apiKey}@tutoringapp-6jldb.mongodb.net/test?retryWrites=true&w=majority`,
-                 { useNewUrlParser: true, useUnifiedTopology: true }).then(result => {console.log("Database connected");app.listen(2900);}).catch(err =>{console.log(err); return res.json({status:false,err})});
+mongoose.connect(`mongodb+srv://lodakins99:${process.env.API_KEY}@tutoringapp-6jldb.mongodb.net/test?retryWrites=true&w=majority`,
+                 { useNewUrlParser: true, useUnifiedTopology: true }).then(result => { app.listen(port);  console.log("Database connected")}).catch(err =>{console.log(err); return res.json({status:false,err})});
